2c76fe8c0f7f9b5afd9a52421828e195f9c27cfd
high-horse <akhileshadhikaria1@gmail.com>
2024-08-27 16:03:00 +0545

    init dir

--- cmd/init.go
+++ cmd/init.go
@@ -24,21 +24,45 @@ var initCommand = &cobra.Command{
 
 func initRepo() {
 	goatDir := ".goat"
-	if err := os.Mkdir(goatDir, 0755); err != nil {
+	if err := os.MkdirAll(goatDir, 0755); err != nil {
 		fmt.Println("Error creating .goat directory:", err)
 		return
 	}
 	
 	dirs := []string{
-		"objects", "refs",
+		"hooks",
+		"info",
+		"objects", 
+		"refs/heads",
+		"refs/tags",
+		"logs/refs/heads",
 	}
 	for _, dir := range dirs {
 		path := filepath.Join(goatDir, dir)
-		if err := os.Mkdir(path, 0755); err != nil {
+		if err := os.MkdirAll(path, 0755); err != nil {
 			fmt.Println("Error creating directory:", err)
 			return
 		}
 	} 
 	
+	files := map[string]string{
+		"HEAD": "ref: refs/heads/main\n",
+		"config": "",
+		"description": "Unnamed repository; edit this file 'description' to name the repository.\n",
+		"info/exclude": "# git ls-files --others --exclude-from=.git/info/exclude\n" +
+			"# Lines that start with '#' are comments.\n" +
+			"# For a project mostly in C, the following would be a good set of\n" +
+			"# exclude patterns (uncomment them if you want to use them):\n" +
+			"# *.[oa]\n" +
+			"# *~\n",
+	}
+	
+	for file, content :=  range files {
+		path := filepath.Join(goatDir, file) 
+		if err := os.WriteFile(path, []byte(content), 0644); err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+	}
 	fmt.Println("Initialized empty directory in ", goatDir)
 }
